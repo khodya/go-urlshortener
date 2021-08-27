0679f9b3ef52269b9cbdaea6ddfd0c943d648944
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-08-27 23:26:29 +0300

    redirect

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func fold(c *gin.Context) {
-	c.IndentedJSON(http.StatusAccepted, nil)
+	url, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil || len(url) == 0 {
+		c.Status(400)
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, url)
 }
 
 func unfold(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, nil)
+	c.Header("Location", "https://www.yandex.com")
+	c.Status(http.StatusTemporaryRedirect)
 }
 
 func main() {
