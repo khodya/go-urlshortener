986a526b8954dac0106bce866f878961174f59d9
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-08-28 00:15:57 +0300

    fix empty scheme 4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -29,7 +29,7 @@ func unfold(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-	router.GET("/:id/*action", unfold)
+	router.GET("/:id", unfold)
 	router.POST("/", fold)
 
 	router.Run("localhost:8080")
