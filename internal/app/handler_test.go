f6c6554e2ffe3c9243407a36c4d0b92dceae509a
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-08-29 00:54:34 +0300

    more tests

--- internal/app/handler_test.go
+++ internal/app/handler_test.go
@@ -20,19 +20,87 @@ func setupRouter() *gin.Engine {
 func TestFold(t *testing.T) {
 	router := setupRouter()
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/", strings.NewReader(""))
-	router.ServeHTTP(w, req)
+	type want struct {
+		code int
+		url  string
+		body string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "happy. fold",
+			want: want{
+				code: 201,
+				url:  "/",
+				body: "https://www.yandex.com",
+			},
+		},
+		{
+			name: "unhappy. fold 404",
+			want: want{
+				code: 404,
+				url:  "/name",
+				body: "https://www.yandex.com",
+			},
+		},
+		{
+			name: "unhappy. fold 400",
+			want: want{
+				code: 400,
+				url:  "/",
+				body: "",
+			},
+		},
+	}
 
-	assert.Equal(t, 400, w.Code)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("POST", tt.want.url, strings.NewReader(tt.want.body))
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.want.code, w.Code)
+		})
+	}
 }
 
 func TestUnfold(t *testing.T) {
 	router := setupRouter()
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/aHR0cHM6Ly93d3cueWFuZGV4LmNvbQ==", nil)
-	router.ServeHTTP(w, req)
+	type want struct {
+		code int
+		url  string
+	}
 
-	assert.Equal(t, 307, w.Code)
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "happy. unfold",
+			want: want{
+				code: 307,
+				url:  "/aHR0cHM6Ly93d3cueWFuZGV4LmNvbQ==",
+			},
+		},
+		{
+			name: "unhappy. unfold",
+			want: want{
+				code: 400,
+				url:  "/aHR0cHM6Ly93d3cueWFuZGLmNvbQ==",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", tt.want.url, nil)
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.want.code, w.Code)
+		})
+	}
 }
