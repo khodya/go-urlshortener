fc58a1269d763e8e5e6067c7a7ae1879ce6c8394
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-08-30 14:57:06 +0300

    renamed package: app -> handlers

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,12 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/khodya/go-urlshortener/internal/app"
+	"github.com/khodya/go-urlshortener/internal/handlers"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/:id", app.Unfold)
-	r.POST("/", app.Fold)
+	r.GET("/:id", handlers.Unfold)
+	r.POST("/", handlers.Fold)
 	return r
 }
