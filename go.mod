44c0f91dea20f23579c607d4d51d62dd0b40e0cd
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-09-16 12:52:12 +0300

    gzip middleware

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/khodya/go-urlshortener
 go 1.16
 
 require (
+	github.com/gin-contrib/gzip v0.0.3
 	github.com/gin-gonic/gin v1.7.4
 	github.com/stretchr/testify v1.7.0
 )
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,15 @@
 package router
 
 import (
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
+
 	"github.com/khodya/go-urlshortener/internal/handlers"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.BestSpeed))
 	r.GET("/:id", handlers.Unfold)
 	r.POST("/", handlers.Fold)
 	r.POST("/api/shorten", handlers.Shorten)
