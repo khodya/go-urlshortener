10a0fc290bc9534d1102ed91ee2bb54d5b428997
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-09-18 11:37:39 +0300

    Revert "guess test 8 again"
    This reverts commit 721e5f28cb685155e82adff8db37dfb473b3951f.

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 
 	"github.com/khodya/go-urlshortener/internal/handlers"
@@ -8,7 +9,7 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	// r.Use(gzip.Gzip(gzip.BestSpeed))
+	r.Use(gzip.Gzip(gzip.BestSpeed))
 	r.GET("/:id", handlers.Unfold)
 	r.POST("/", handlers.Fold)
 	r.POST("/api/shorten", handlers.Shorten)
