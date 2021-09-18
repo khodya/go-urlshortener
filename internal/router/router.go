bbb5b23ec34b09df0413ebe346aa3be927f89224
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-09-18 18:13:10 +0300

    gzip middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,7 +9,7 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(gzip.Gzip(gzip.BestSpeed))
+	r.Use(gzip.Gzip(gzip.BestSpeed, gzip.WithDecompressFn(gzip.DefaultDecompressHandle)))
 	r.GET("/:id", handlers.Unfold)
 	r.POST("/", handlers.Fold)
 	r.POST("/api/shorten", handlers.Shorten)
