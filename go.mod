ff951ea3f84f9939fa17fd1bad430682c4977993
Andrey Khodyrev <akhodyrev@griddynamics.com>
2021-09-22 14:13:34 +0300

    Revert "add cookies unencrypted"
    This reverts commit 6502ed05a123af102f9cc490f08f722f98da410e.

--- go.mod
+++ go.mod
@@ -5,6 +5,5 @@ go 1.16
 require (
 	github.com/gin-contrib/gzip v0.0.3
 	github.com/gin-gonic/gin v1.7.4
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
 )
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,27 +3,15 @@ package router
 import (
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 
 	"github.com/khodya/go-urlshortener/internal/handlers"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(Cookie())
 	r.Use(gzip.Gzip(gzip.BestSpeed, gzip.WithDecompressFn(gzip.DefaultDecompressHandle)))
 	r.GET("/:id", handlers.Unfold)
 	r.POST("/", handlers.Fold)
 	r.POST("/api/shorten", handlers.Shorten)
 	return r
-}
-
-func Cookie() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		_, err := c.Cookie("user")
-		if err != nil {
-			c.SetCookie("user", uuid.NewString(), 300, "", "", true, true)
-		}
-		c.Next()
-	}
 }
